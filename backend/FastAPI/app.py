b9ed0985be5a53f09e615256f1edc9204e6dea20
machine <appletreegood123@naver.com>
2022-06-29 14:56:50 +0900

    images

--- backend/FastAPI/app.py
+++ backend/FastAPI/app.py
@@ -1,19 +1,18 @@
-import uvicorn 
-from fastapi import FastAPI, Request 
-from fastapi.responses import HTMLResponse 
-from fastapi.staticfiles import StaticFiles 
-from fastapi.templating import Jinja2Templates 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI() 
-templates = Jinja2Templates(directory="templates") 
-app.mount("/static", StaticFiles(directory="static"), name="static") 
+app = FastAPI()
 
-@app.get('/') 
+app.mount("../../frontend/static", StaticFiles(directory="static"), name="static")
+
+@app.get('/')
 def hello_world(): 
 	return {'message':'안녕하세요'} 
 
-@app.get("/items/{id}", response_class=HTMLResponse) 
-async def read_item(request: Request, id: str): 
-	return templates.TemplateResponse("item.html", {"request": request, "id":id}) 
+@app.get("/home.html", response_class=HTMLResponse) 
+async def read_item(request: Request): 
+	return templates.TemplateResponse("home.html") 
 
 if __name__ == '__main__': uvicorn.run(app)
