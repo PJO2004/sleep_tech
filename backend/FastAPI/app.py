8b83c71ae8ed7f91e0b421d64041a33ffd5eb36d
machine <appletreegood123@naver.com>
2022-07-01 07:40:27 +0900

    static

--- backend/FastAPI/app.py
+++ backend/FastAPI/app.py
@@ -11,10 +11,20 @@ templates = Jinja2Templates(directory="../../frontend/templates")
 def hello_world(): 
 	return templates.TemplateResponse('home.html') 
 
-if __name__ == '__main__': uvicorn.run(app)
+def create_app():
+    """
+    앱 함수 실행
+    :return:
+    """
 
+    app = FastAPI()
+    app.mount("../../frontend/static/css", StaticFiles(directory="static"), name="static")
 
-# @router.get("/", response_class=HTMLResponse)
-# async def home(request: Request):
-#     data = openfile("home.md")
-#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
+    # 라우터 정의
+    app.include_router(index.router)
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
