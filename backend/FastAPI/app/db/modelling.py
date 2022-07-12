9f39c7f79c2ac0066832eedbcacb00a985b96a95
machine <appletreegood123@naver.com>
2022-07-12 14:21:57 +0900

    :art:코드-마무리

--- backend/FastAPI/app/db/modelling.py
+++ backend/FastAPI/app/db/modelling.py
@@ -16,9 +16,7 @@ def file_modeling():
     print('file_modeling')
     # csv 데이터 저장
     tn, df, columns = reload()
-    print('reload 완료')
     # table 생성
-    print('table 생성')
     index = table_modeling(tn=tn, df=df, columns=columns)
     return tn, df, columns, index
 
@@ -33,7 +31,6 @@ def table_modeling(tn="", df=None, columns=None):
     index = False
 
     # 각 컬럼에 알맞는 table 컬럼 작성
-    print('table컬럼 생성')
     for cnt, d in enumerate(columns):
         m = len(str(df[d][0]))
         memory = int(m+(m/2))
@@ -67,19 +64,17 @@ def table_modeling(tn="", df=None, columns=None):
 
 # csv 파일 데이터 로 변환
 def reload():
-    print('reload시작')
     table_name = next(open("./app/data/filename.txt", "r"))
     df = pd.read_csv(f"./app/data/data.csv", encoding='cp949')
-    print('번역')
     # for i, col in enumerate(df.columns):
     #     df = df.rename(columns={
     #         col: get_translate(col)
     #     })
     columns_ = df.columns
-    print('reload 완')
     return table_name.lower(), df, columns_
 
 
+# 번역
 def get_translate(text):
     client_id, client_secret = "LnbkGpZ1FltpJ_Jytof0", "2odQqgaDim" # [ADD] Naver API developer ID
     url = "https://openapi.naver.com/v1/papago/n2mt"
